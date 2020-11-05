672c0184af6593df32e5762f49fa39ed12cb2861
ChristianPucci91 <pucci.christian91@gmail.com>
2020-11-05 21:36:31 +0100

    Esercizio completo, manca stampa su DOM

--- js/script.js
+++ js/script.js
@@ -2,70 +2,68 @@
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
-// STEP 1 generiamo 5 numeri casuali con una funzione e li pushiamo dentro un array che verrà visualizzato all'utente
-// -- non devono ripetersi -- -- devono essere casuali -- -- devono essere visualizzati in un prompt --
+// >>>>>>>>>>>>>>> INIZIO ESERCIZIO <<<<<<<<<<<<<<<<
 
-// dichiaro una variabile array per i numeri casuali
-var numeriGenerati = [];
-numeriGenerati = (generaNumeri(1,100));
-console.log("i numeri generati sono:", numeriGenerati);
-// STEP 2 l'utente dovrà ricordare i 5 numeri usciti e dovrà poi dopo 30 secondi inserirli all'interno di un prompt ripetuto per 5 volte.
+// -------------------------------------------------------------------------------------------
+var numeriGenerati = []; // dichiaro una variabile array per i numeri casuali
+var punteggio = 0; // creo variabile del punteggio giocatore
+numeriGenerati = (generaNumeri(1,100)); // richiamo la funzione generaNumeri
+console.log("I numeri che devi ricordare sono: ",numeriGenerati);
+alert("I Numeri che devi ricordare sono: " + numeriGenerati) // creo alert con i numeri generati dalla funzione
+// -------------------------------------------------------------------------------------------
 
-// ci sarà quindi una funzione setTimeout e all'interno un ciclo for a cui chiediamo all'utente di inserire per 5 volte un numero con i vari
-// casi limite.
+// creo funzione setTimeout che si attiverà 30 secondi dopo che l'utente avrà dato l'ok a l'alert dei numeriGenerati
+// questa funzione servirà per far inserire i numeri all'utente e pusharli dentro a 2 array.
+setTimeout(function () {
 
-// creo variabile del punteggio giocatore
-var punteggio = 0;
+  var arrayUtente = []; // array per i 5 numeri inseriti dall'utente
+  var numeriIndovinati = []; // array dei numeri che l'utente indovina
 
-var partita = setTimeout(function () {
+  for (var i = 0; i < 5; i++) { // ciclo for per chiedere all'utente 5 volte di inserire un numero
 
-   var arrayUtente = [];
-   var numeriIndovinati = [];
-   for (var i = 0; i < 5; i++) {
-     var numeroInserito = [i];
+    var numeroInserito = [i];
+    numeroInserito = parseInt(prompt("inserisci numero"));
+    // casi limite di inserimento numeri utente
+    while ((arrayUtente.includes(numeroInserito)) || (isNaN(numeroInserito)) || (numeroInserito > 100) || (numeroInserito < 1) ) {
+      numeroInserito = parseInt(prompt("Per favore, inserisci un numero secondo le regole!"));
+    }
+    arrayUtente.push(numeroInserito);
+    console.log("array utente",arrayUtente);
+    // condizione se il numero inserito dall'utente è all'interno dell'array dei numeriGenerati aumento il punteggio di 1 e
+    // lo inserisco all'interno dell'array dei numeriIndovinati
+    if (numeriGenerati.includes(numeroInserito)) {
+      punteggio++;
+      numeriIndovinati.push(numeroInserito);
+    }
+   console.log("il tuo punteggio è",punteggio); // stampo punteggio
+  }// fine ciclo for
 
-     numeroInserito = parseInt(prompt("inserisci numero"));
+  // condizione dell'esito partita. se il punteggio è < 5 il giocatore ha perso
+  // altrimenti ha vinto.
+  if (punteggio < 5) {
+   console.log("Hai perso!" + "il tuo punteggio è: ",punteggio);
+   console.log("i numeri che hai indovinato sono: ",numeriIndovinati);
+  } else {
+   console.log("Hai vinto!!" + "il tuo punteggio è:",punteggio);
+   console.log("I numeri che hai indovinato sono: ", arrayUtente);
+  }
 
-     while ((arrayUtente.includes(numeroInserito)) || (isNaN(numeroInserito)) || (numeroInserito > 100) || (numeroInserito < 1) ) {
-       numeroInserito = parseInt(prompt("Per favore, inserisci un numero secondo le regole!"));
-     }
-     arrayUtente.push(numeroInserito);
-     console.log("array utente",arrayUtente);
-     if (numeriGenerati.includes(numeroInserito)) {
-       punteggio++;
-       numeriIndovinati.push(numeroInserito);
-     }
-     console.log("il tuo punteggio è",punteggio);
-   }
-
-   // STEP 3 una volta inseriti il programma dovrà stabilire quanti numeri inseriti dall'utente sono uguali all'array dei numeri casuali
-   // e dichiarare il punteggio. se punteggio == a 5 l'utente avrà vinto altrimenti punteggio sarà uguale ai numeri indovinati
-
-   // STEP 4 OUTPUT il programma stamperà il punteggio e quali numeri sono stati azzeccati
-
-   if (punteggio < 5) {
-     console.log("Hai perso!" + "il tuo punteggio è: ",punteggio);
-     console.log("i numeri che hai indovinato sono: ",numeriIndovinati);
-   }else {
-     console.log("Hai vinto!!" + "il tuo punteggio è:",punteggio);
-     console.log("I numeri che hai indovinato sono: ", arrayUtente);
-   }
-
-
-},1000)
+},1000) // fine funzione setTimeout
 
 
 // FUNZIONI
 
-// funzione che genera numeri random da 1 a 100 con un ciclo che controlla se ci sono numeri uguali
+// funzione che genera numeri random da 1 a 100 con un ciclo while che controlla se ci sono numeri uguali
 function generaNumeri(min,max) {
 
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 5; i++) { // inizio ciclo for
+
     numeriGeneratiElemento = Math.floor(Math.random()* max )+min;
+
     while (numeriGenerati.includes(numeriGeneratiElemento)) {
       numeriGeneratiElemento = Math.floor(Math.random()* max) +min;
     }
     numeriGenerati.push(numeriGeneratiElemento);
-  }
+  } // fine ciclo for
   return numeriGenerati;
-}
+} // fine funzione
